d8b32b4e13c36baba7f9c1714d446b96067af79a
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-11-30 19:51:01 +0800

    Add db config

---
+++ includes/db-config.php
@@ -0,0 +1,13 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'mydb';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
