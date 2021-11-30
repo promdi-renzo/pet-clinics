c6c1db8002ca81ac4a0882b30ab09b2efb70e1f9
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-11-30 19:20:08 +0800

    Add header

---
+++ component/header.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="logo">
+        <h1>Pet <span>Clinic</span></h1>
+    </div>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Pets</li>
+            <li>Employees</li>
+            <li>Services</li>
+            <li>Transactions</li>
+        </ul>
+    </nav>
+</header>
