c92eb52a59833fd27c079e16118862254e623c3f
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-11-30 19:07:20 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require('./includes/meta.php')
+?>
+
+<body>
+    <div class="login-viewer">
+        <div class="login">
+            <div class="login__logo">
+                <h1>Lo<span>gin</span></h1>
+            </div>
+            <form action="" class="login__form">
+                <div class="login__input">
+                    <label for="usn">Username</label>
+                    <input type="text" name="usn" id="usn">
+                </div>
+                <div class="login__input">
+                    <label for="psw">Password</label>
+                    <input type="password" name="psw" id="psw">
+                </div>
+                <button type="submit"> Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
